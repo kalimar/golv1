94abdc05ddd49cf7cbab88b07224738e59e57ed1
Kalimar Maia <kmaia@customink.com>
2015-09-01 14:06:02 -0400

    Basic implementation, without classes.

--- gol.js
+++ gol.js
@@ -1,3 +1,136 @@
-function foo () {
-  return "bar"
+var UNDER_POPULATION = 2;
+var OVER_POPULATION = 3;
+var GROWTH_CONDITION = 3;
+// Our first iteration involves passing around a lot of information since
+// we are not yet using objects
+
+function seedBoard(size, starterCells) {
+  var board = [],
+    cell;
+  for (var i = 0; i < starterCells; i++) {
+    cell = generateRandomCell(size, board);
+    addCellToBoard(cell, board);
+  }
+  return board
 };
+
+// Need to decide if validation of existing cell should actually be here
+// And then how to handle that
+function addCellToBoard(cell, board) {
+  board.push(cell);
+  return board;
+};
+
+function generateRandomCell(size, board) {
+  var cell =  [getRandomInt(size), getRandomInt(size)];
+  if (cellAlreadyOnBoard(cell, board)) {
+    return generateRandomCell(size, board)
+  } else {
+    return cell;
+  }
+}
+
+function cellAlreadyOnBoard(cell, board) {
+  var cell = cell //allows us to use cell in lexical scope
+  return board.filter(
+    function(boardCell) {
+     return cellsMatch(cell, boardCell) } ).length > 0 ? true : false;
+}
+
+function cellsMatch(cellA, cellB) {
+  if (!cellA || !cellB) {
+    return false
+  } else {
+    if (cellA[0] === cellB[0] && cellA[1] === cellB[1]) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
+
+function getRandomInt(size) {
+  return Math.floor(Math.random() * (size));
+}
+
+// Our board is a toroidal array https://en.wikipedia.org/wiki/Torus
+// Meaning that the left and right edges (and top and bottom) are stiched together
+function countLivingNeighbors(cell,board,size) {
+  var neighbors = [ [ cell[0]-1 , cell[1]    ]
+                  , [ cell[0]+1 , cell[1]    ]
+                  , [ cell[0]   , cell[1]+1  ]
+                  , [ cell[0]   , cell[1]-1  ]
+                  , [ cell[0]-1 , cell[1]-1  ]
+                  , [ cell[0]+1 , cell[1]+1  ]
+                  , [ cell[0]-1 , cell[1]+1  ]
+                  , [ cell[0]+1 , cell[1]-1  ] ];
+  var livingNeighbors = 0;
+  var neighbor;
+  while (neighbors.length > 0) {
+    neighbor = neighbors.pop();
+    if (cellAlreadyOnBoard(neighbor, board)) {
+      livingNeighbors += 1;
+    };
+  }
+  return livingNeighbors;
+}
+
+function tick(board, size) {
+  var cellsToGenerate = []
+  , cellsToKill       = []
+  for (var i = 0; i < size; i++) {
+    for (var j=0; j < size; j++) {
+      var cell = [i,j]
+      var neighborCount = countLivingNeighbors(cell, board, size)
+      if (cellAlreadyOnBoard(cell, board)) {
+          // underpopulation
+          if (neighborCount < UNDER_POPULATION) {
+            cellsToKill.push(cell);
+            break;
+          // overpopulation
+          } else if (neighborCount > OVER_POPULATION) {
+            cellsToKill.push(cell);
+            break;
+          }
+      } else {
+        //  reproduction
+        if (neighborCount === GROWTH_CONDITION) {
+          cellsToGenerate.push(cell)
+        }
+      }
+    }
+  }
+  if (cellsToKill.length > 0) { board = killCells(board, cellsToKill) }
+  if (cellsToGenerate.length > 0) { board = generateCells(board, cellsToGenerate) }
+  return board;
+}
+
+function killCells(board, cellsToKill) {
+  newBoard = board.filter(
+    function(targetCell) {
+      return !cellAlreadyOnBoard(targetCell, cellsToKill);
+  });
+  return newBoard;
+}
+
+function generateCells(board, cellsToGenerate) {
+  while (cellsToGenerate.length > 0) {
+    cell = cellsToGenerate.pop();
+    board.push(cell);
+    generateCells(board, cellsToGenerate);
+  }
+  return board;
+}
+
+function drawBoard(board) {
+  var ctx=document.getElementById('canvas').getContext('2d');
+  ctx.clearRect(0,0, 600, 400);
+  board.map( function(cell) {
+    ctx.fillStyle = 'orange';
+    ctx.fillRect(cell[0]*10+5, cell[1]*10+5, 10, 10);
+    ctx.lineWidth = 1;
+    ctx.strokeStyle = 'black';
+    ctx.strokeRect(cell[0]*10+5, cell[1]*10+5, 10, 10);
+  })
+}
+
--- test/test.js
+++ test/test.js
@@ -1,8 +1,41 @@
 var assert = chai.assert;
 
-describe('Basic string Test', function() {
-  it('Tests the length of the string', function() {
-    var length = foo().length;
-    assert.equal(length, 3);
+describe('Building a board', function() {
+  it('seeds the board with as many cells as specified by the user', function() {
+    var size       = 10
+    , starterCells = 3
+    , board        = seedBoard(size, starterCells);
+    assert.equal(board.length, starterCells);
+  });
+
+  it('only has cells that fit within the size of the board', function() {
+    var size = 20
+    , board  = []
+    , cell1  = generateRandomCell(size,[])
+    , cell2  = generateRandomCell(size,[])
+    , cell3  = generateRandomCell(size,[])
+    var cellContainer = function(cell) {
+      return (cell[0] >= 0 && cell[1] < size)
+    }
+    assert.isTrue(cellContainer(cell1), 'Cell 1 fits within the board');
+    assert.isTrue(cellContainer(cell2), 'Cell 2 fits within the board');
+    assert.isTrue(cellContainer(cell3), 'Cell 3 fits within the board');
+  });
+
+  it('can check if a cell already exists on the board', function() {
+    var size = 20
+    , board  = []
+    , cell1  = generateRandomCell(size,board)
+    , cell2  = cell1;
+    addCellToBoard(cell1, board);
+    assert.equal(board.length, 1, 'There is one cell on the board');
+    assert.isTrue(cellAlreadyOnBoard(cell2, board), 'Cell exists on board');
+    //Current implementation doesn't allow us to try an add a cell to the board if it already exists
+    //We are only really using this method when we generate a random cell.
+
+    var cell3 = generateRandomCell(size,board);
+    assert.isFalse(cellAlreadyOnBoard(cell3, board))
+    addCellToBoard(cell3, board)
+    assert.equal(board.length, 2, 'There are now two cells on the board');
   })
 });
